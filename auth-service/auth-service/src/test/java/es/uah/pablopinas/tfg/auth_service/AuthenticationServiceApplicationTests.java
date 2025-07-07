dd775ba48b59e5bce473b02c0098b62b50071560
Pablo Piñas Andrés <osoyinas@gmail.com>
2025-07-07 23:27:05 +0200

    Add auth-service microservice

---
+++ auth-service/auth-service/src/test/java/es/uah/pablopinas/tfg/auth_service/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uah.pablopinas.tfg.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
