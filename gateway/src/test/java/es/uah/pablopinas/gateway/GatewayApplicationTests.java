49765384863ff09db43919a90dfee1c10f314460
Pablo Piñas Andrés <osoyinas@gmail.com>
2025-08-09 00:26:19 +0200

    Add gateway config

---
+++ gateway/src/test/java/es/uah/pablopinas/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uah.pablopinas.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
