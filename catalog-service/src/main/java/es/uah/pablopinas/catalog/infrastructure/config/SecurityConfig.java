a8d272a03cdc91a5a979ae68c91d0d921e461270
Pablo Piñas Andrés <osoyinas@gmail.com>
2025-08-15 12:58:06 +0200

    Add Spring Boot Actuator dependency and configure security for actuator endpoints

--- catalog-service/src/main/java/es/uah/pablopinas/catalog/infrastructure/config/SecurityConfig.java
+++ catalog-service/src/main/java/es/uah/pablopinas/catalog/infrastructure/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package es.uah.pablopinas.catalog.infrastructure.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,21 +21,31 @@ public class SecurityConfig {
     public final static String ADMIN_ROLE = "CATALOG_ADMIN";
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+    @Order(0)
+    SecurityFilterChain actuatorChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher(EndpointRequest.toAnyEndpoint()) // todos los endpoints de actuator
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+    @Bean
+    @Order(1)
+    SecurityFilterChain apiChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .cors(cors -> cors.disable()) // Disable CORS
+                .cors(cors -> cors.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole(ADMIN_ROLE)
+                        .requestMatchers("/swagger/**").permitAll()   // si quieres
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt
-                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
-                        )
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
                 );
-
         return http.build();
     }
 
