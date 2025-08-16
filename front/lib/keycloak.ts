2e877808204a5c06b2c202279cc9c3a9152532b5
Pablo Piñas Andrés <osoyinas@gmail.com>
2025-08-16 23:58:13 +0200

    Remove silentCheckSsoRedirectUri from Keycloak configuration

--- front/lib/keycloak.ts
+++ front/lib/keycloak.ts
@@ -4,7 +4,6 @@ import Keycloak from "keycloak-js";
 const keycloakConfig = {
   pkceMethod: "S256",
   onLoad: "check-sso",
-  silentCheckSsoRedirectUri: `${window?.location.origin}/silent-check-sso.html`,
   url: process.env.NEXT_PUBLIC_KEYCLOAK_URL!!,
   realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM!!,
   clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID!!,
