a4541ec2f45a21b051f9fc27fe72e97812d632f7
Pablo Piñas Andrés <osoyinas@gmail.com>
2025-08-16 23:56:04 +0200

    Fix optional chaining for silentCheckSsoRedirectUri in Keycloak configuration

--- front/lib/keycloak.ts
+++ front/lib/keycloak.ts
@@ -4,7 +4,7 @@ import Keycloak from "keycloak-js";
 const keycloakConfig = {
   pkceMethod: "S256",
   onLoad: "check-sso",
-  silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`,
+  silentCheckSsoRedirectUri: `${window?.location.origin}/silent-check-sso.html`,
   url: process.env.NEXT_PUBLIC_KEYCLOAK_URL!!,
   realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM!!,
   clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID!!,
