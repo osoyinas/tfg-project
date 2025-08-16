314c3449dd1a9fc9e4b5d0b3b9b14f7a75efd09f
Pablo Piñas Andrés <osoyinas@gmail.com>
2025-08-16 23:50:42 +0200

    Add missing Keycloak configuration properties for pkceMethod and onLoad

--- front/lib/keycloak.ts
+++ front/lib/keycloak.ts
@@ -2,6 +2,8 @@ import Keycloak from "keycloak-js";
 
 // Configuración de ejemplo, reemplaza con tus valores reales
 const keycloakConfig = {
+  pkceMethod: "S256",
+  onLoad: "check-sso",
   url: process.env.NEXT_PUBLIC_KEYCLOAK_URL!!,
   realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM!!,
   clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID!!,
